d98a8ce3154c3687a5138555605453ec427eb78a
dxftctcdtc <tctcdtc@gmail.com>
2014-02-10 14:25:36 -0500

    Create Jump_Game.py

---
+++ Jump_Game.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Feb 10, 2014
+
+@author: Xufan Du, tctcdtc@gmail.com
+
+'''
+
+class Solution:
+    # @param A, a list of integers
+    # @return a boolean
+    d={}
+    def canJump(self, A):
+        #no element, cannot reach the 'last one'
+        if len(A)==0: return False
+        #if only one element, can reach the last one
+        if len(A)==1: return True
+        B = [False for i in range(0,len(A))]
+        B[0]=True
+        m=0
+        for i in range(0,len(A)):
+            if B[i] or i<=m:
+                m=m if m>(A[i]+i) else (A[i]+i)
+            if m>=len(A)-1:
+                return True
+        return False
+    
+if __name__ == '__main__':
+    s = Solution()
+    print s.canJump([3,2,1,0,4])
+
