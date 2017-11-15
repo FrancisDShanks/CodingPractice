64e5a87f5c5c368e180a5843b5d627022d54bcc2
FrancisDShanks <tctcdtc@gmail.com>
2017-11-15 15:20:00 -0700

    Update construct_string_from_binary_tree.py

--- LeetCode/construct_string_from_binary_tree.py
+++ LeetCode/construct_string_from_binary_tree.py
@@ -1,4 +1,4 @@
-#straight foward solution
+#straight foward solution using recursion
 class Solution:
     def tree2str(self, t):
         """
@@ -21,3 +21,52 @@ class Solution:
         else:
             tright = '' 
         return str(t.val) + tleft + tright
+
+    
+    
+    
+    
+    
+#using the idea of stack, do the pre-order 
+class Solution:
+    def tree2str(self, t):
+        """
+        :type t: TreeNode
+        :rtype: str
+        """
+        if not t:
+            return ''
+        stack = [t]
+        ret = ''
+        
+        while stack:
+            pop = stack.pop()
+            if pop == '(' or pop == ')':
+                ret += pop
+            else:
+                if not pop.left and not pop.right:
+                    ret += str(pop.val)
+                else:
+                    ret += str(pop.val)
+                    if pop.right:
+                        stack.append(')')
+                        stack.append(pop.right)
+                        stack.append('(')
+                        
+                    stack.append(')')
+                    if pop.left:
+                        stack.append(pop.left)
+                    stack.append('(')
+
+        return ret
+    
+    
+    
+    
+    #4-lines solution from leetcode discussion, using the same algorithm as my 1st solution
+    class Solution:
+    def tree2str(self, t):
+        if not t: return ''
+        left = '({})'.format(self.tree2str(t.left)) if (t.left or t.right) else ''
+        right = '({})'.format(self.tree2str(t.right)) if t.right else ''
+        return '{}{}{}'.format(t.val, left, right)
