2f8c8c30ec41c0abf5a10f6b1c51474cc042b13c
FrancisDShanks <tctcdtc@gmail.com>
2017-11-22 12:27:24 -0700

    Create count_and_say.py

---
+++ LeetCode/count_and_say.py
@@ -0,0 +1,36 @@
+class Solution:
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        term = '1'
+        for i in range(1,n):
+            #notice here!!!!!!!!!!!!!!
+            count = 0
+            curchar = term[0]
+            countchar = curchar
+            newterm = ''
+            for j in range(0, len(term)):
+                curchar = term[j]
+                if j == len(term) - 1:
+                    if countchar == curchar:
+                        count += 1
+                    else:
+                        newterm += (str(count) + countchar)
+                        count = 1
+                    newterm += (str(count) + curchar)
+                    
+                else:
+                    if countchar == curchar:
+                        count += 1
+                    else:
+                        newterm += (str(count) + countchar)
+                        #notice here!!!!!!!!!!!!!! should reset to 1
+                        count = 1
+                        countchar = curchar
+            term = newterm
+                        
+        return term
+                
+                
