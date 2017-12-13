07455465dd1bf019f4271e96d81df3a0784347fa
FrancisDShanks <tctcdtc@gmail.com>
2017-12-12 17:04:19 -0700

    Update binary_tree_level_order_traversal.py

--- LeetCode/binary_tree_level_order_traversal.py
+++ LeetCode/binary_tree_level_order_traversal.py
@@ -1,3 +1,5 @@
+#using one queue
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -34,3 +36,42 @@ class Solution:
         
         #res.append(curlvl)
         return res
+
+    
+    
+#using two queues, faster    
+    
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        res = []
+        lvlres = []
+        if not root:
+            return []
+        curlvl = [root]
+        nextlvl = []
+        
+        while curlvl:
+            while curlvl:
+                cur = curlvl.pop(0)
+                if cur.left:
+                    nextlvl.append(cur.left)
+                if cur.right:
+                    nextlvl.append(cur.right)
+                lvlres.append(cur.val)
+            res.append(lvlres)
+            lvlres = []
+            curlvl,nextlvl = nextlvl,curlvl
+
+        return res
+        
