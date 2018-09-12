ab55e32606a8d186601f57e6dafc2df0ece3fd4a
FrancisDShanks <tctcdtc@gmail.com>
2018-09-12 11:14:36 +0800

    Create repeated_substring_pattern_100%.py

---
+++ LeetCode/459. Repeated_Substring_Pattern/repeated_substring_pattern_100%.py
@@ -0,0 +1,18 @@
+# a 100% solution found in leetcode discussion
+# 这个算法的原理想了很久还是有些迷糊,大概的理解如下:
+# 因为根据提交情况可以知道,s本身不能作为自己的substring
+# 所以符合pattern的s至少由2个或以上sub重复来构成
+# 以s由2个sub构成为例:
+# ss = s[1:]+s[:-1], ss由2个s组成,也就是由4个sub组成
+# 去掉头尾的字符,破坏第一个sub和最后一个sub,中间应该2个sub,恰好构成s
+# 所以如果s in ss就返回True
+class Solution:
+    def repeatedSubstringPattern(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        ss = s[1:]+s[:-1]
+        return s in ss
+                
+        
