1dc7dec86aec028424b9d97e1ab964df621635fc
FrancisDShanks <tctcdtc@gmail.com>
2019-07-04 10:56:07 +0800

    Update maximum_average_subarray_i.py

--- LeetCode/643. Maximum Average Subarray I/maximum_average_subarray_i.py
+++ LeetCode/643. Maximum Average Subarray I/maximum_average_subarray_i.py
@@ -2,9 +2,7 @@ class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         if not nums or k > len(nums) :
             return 0
-        res = 0
-        for i in range(0, k):
-            res += nums[i]
+        res = sum(nums[:k])
         m = res  # initial m as the sum of first subarray. in case of the array only has one element and the loop is not executed.
         for i in range(1, len(nums) - k + 1):
             res = res - nums[i - 1] + nums[i + k - 1]
