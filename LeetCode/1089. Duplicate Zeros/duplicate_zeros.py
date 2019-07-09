8fa44d9151d743fbeea88063a5e8d01a08b6e16d
FrancisDShanks <tctcdtc@gmail.com>
2019-07-09 11:36:49 +0800

    Update duplicate_zeros.py

--- LeetCode/1089. Duplicate Zeros/duplicate_zeros.py
+++ LeetCode/1089. Duplicate Zeros/duplicate_zeros.py
@@ -14,3 +14,23 @@ class Solution:
             arr.pop()
             
             
+# solution - not using python list.insert()
+# much slower, shows that the python list.insert() is well designed
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        i = 0
+        l = len(arr)
+        while i < l:
+            if not arr[i]:
+                j = l - 1
+                while j > i:
+                    arr[j] = arr[j - 1]
+                    j -= 1
+                i += 1
+                    
+            i += 1
+        while len(arr) > l:
+            arr.pop()
