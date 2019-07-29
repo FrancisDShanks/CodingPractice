c85e7ecdf1ec2acdf10a072ce84b02f3189878c3
FrancisDShanks <tctcdtc@gmail.com>
2019-07-29 16:07:11 +0800

    Create stream_of_characters.py

---
+++ LeetCode/1032. Stream of Characters/stream_of_characters.py
@@ -0,0 +1,34 @@
+# solution - 1, implement trie tree with defaultdict
+from collections import defaultdict
+Trie = lambda: defaultdict(Trie)
+
+class StreamChecker:
+
+    def __init__(self, words: List[str]):
+        self.Trie = Trie()
+        for word in words:
+            cur = self.Trie
+            for c in word:
+                cur = cur[c]
+            cur['end'] = True
+        self.trie_nodes = [self.Trie] # 维护已经有匹配的前缀结点
+        
+
+    def query(self, letter: str) -> bool:
+        tmp = self.trie_nodes
+        self.trie_nodes = [self.Trie] # 保持Trie跟在里面(代表从头匹配)
+        flag = False
+        for node in tmp:
+            if letter not in node: 
+                continue
+            self.trie_nodes.append(node[letter])
+            if 'end' in node[letter]:
+                flag = True # 标记flag而不是马上返回,要过完所有tmp
+        return flag
+                
+        
+
+
+# Your StreamChecker object will be instantiated and called as such:
+# obj = StreamChecker(words)
+# param_1 = obj.query(letter)
