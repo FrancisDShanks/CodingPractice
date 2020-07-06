7756b66734a23a0cccc6a9bc7ff6b8e44ea592c5
FrancisDShanks <tctcdtc@gmail.com>
2020-07-06 16:03:41 +0800

    Update balanced_binary_tree.cpp

--- LeetCode_cpp/110. Balanced Binary Tree/balanced_binary_tree.cpp
+++ LeetCode_cpp/110. Balanced Binary Tree/balanced_binary_tree.cpp
@@ -11,33 +11,29 @@
  */
 class Solution {
 public:
-    int helper(TreeNode* node, bool *isbt) {
-        if (node->left == null and node->right == null){
-            isbt = true;
+    bool isbt;
+    int helper(TreeNode* node) {
+        if (!isbt) return 0;
+        if (!node) return 0;
+        
+        int rh = helper(node->right);
+        int rl = helper(node->left);
+        if (!isbt) return 0; //这里加一个判断从28ms 60%提升到16ms 90%
+        
+        
+        int dif = abs(rh - rl);
+        
+        if (dif > 1){
+            isbt = false;
             return 0;
         }
-        
-        bool isr = false;
-        bool isl = false;
-        int rh = helper(node->right, isr);
-        int rl = helper(node->left, isl);
-        
-        if (isr == false or isl == false) return 0;
-        int dif = rh - rl;
-        if (dif < 0) dif = dif * (-1);
-        if (dif == 0 or dif == 1){
-            isbt = true;
-            return dif;
-        }
-        else{
-            return 0;
-        }
+        return max(rh, rl) + 1;
             
     }
     
     bool isBalanced(TreeNode* root) {
-        bool isbt = false;
-        int height = helper(root, isbt);
+        isbt = true;
+        int height = helper(root);
         return isbt;   
         
     }
